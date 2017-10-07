bbc91d67d6157d7d7ef9ca9aec2c7a36f61b0924
kiran4399 <klekkala@usc.edu>
2017-10-06 23:27:35 -0700

    working code updated

--- rand_matrix.cpp
+++ rand_matrix.cpp
@@ -11,26 +11,28 @@ int main(){
 	std::string base(".txt");
 	int ind = 0;
 	while(1){
-	int n = (rand() % 22) + 5;
-	int f = (rand() % 8) + 2;
-	int counter = 0;
-	std::ofstream outfile(std::to_string(ind++)+base);
-	std::vector<char> v;
-
-	for(int j = 0;j < n*n; j++){
-		v.push_back('0'+ (rand() % f));
-	}
-
-	std::random_shuffle(v.begin(), v.end());
-
-	for(int j=0;j<n*n;j++){
-		if(counter++ == n){
-			outfile << std::endl;
-			counter = 1;
+		int n = (rand() % 22) + 5;
+		int f = (rand() % 8) + 2;
+		int counter = 0;
+		std::ofstream outfile(std::to_string(ind++)+base);
+		std::vector<char> v;
+		outfile << n << std::endl;
+		outfile << f << std::endl;
+		outfile << 214748 << std::endl;
+		for(int j = 0;j < n*n; j++){
+			v.push_back('0'+ (rand() % f));
 		}
-		outfile << v.back();
-		v.pop_back();
+
+		std::random_shuffle(v.begin(), v.end());
+
+		for(int j=0;j<n*n;j++){
+			if(counter++ == n){
+				outfile << std::endl;
+				counter = 1;
+			}
+			outfile << v.back();
+			v.pop_back();
+		}
 	}
-}
 
 }
